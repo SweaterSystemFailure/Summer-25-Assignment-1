1fdd295dec2ab3cd47b2ba4efbc8387bf738cd1e
Ben <benjamin.s.fagan@gmail.com>
2025-06-18 13:14:34 -0700

    initial commit

---
+++ hello_inventory.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello Inventory System!\n";
+    cout << endl;
+    cout << "Data Types:\n";
+    cout << "\tInts contain " << sizeof(int) << " bytes.\n";
+    cout << "\tFloats contain " << sizeof(float) << " bytes.\n";
+    cout << "\tChars contain " << sizeof(char) << " byte.\n";
+
+    return 0;
+}
+
